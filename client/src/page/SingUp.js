0d06b809cd953672d6b643ab4eb1a64138db04c2
abdelhadi22k <115563437+abdelhadi22k@users.noreply.github.com>
2024-08-29 15:45:40 +0100

    Update SingUp.js

--- client/src/page/SingUp.js
+++ client/src/page/SingUp.js
@@ -8,6 +8,7 @@ import domain from "../utils/config";
 function SingUp() {
   const navigate = useNavigate(); 
 
+  
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
