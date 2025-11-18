7e5e2aeb5e2ba9075903502008f84b19ec07234e
abdelhadi22k <115563437+abdelhadi22k@users.noreply.github.com>
2025-11-18 16:22:06 +0100

    Update config.js

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,3 +1,6 @@
-var domain = https://pc-store-acos-back-end.vercel.app
+// src/utils/config.js
+
+// دومين الـ backend الخاص بك
+const domain = "https://pc-store-acos-back-end.vercel.app";
 
 export default domain;
